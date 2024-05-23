6a5cb218dec78c9818dbf76756897080b27418f4
sostar <sostar0832@cau.ac.kr>
2024-05-23 15:39:16 +0900

    AccountModel 수정 및 테스트케이스 생성

--- SE_Term_Project/src/main/model/AccountModel.java
+++ SE_Term_Project/src/main/model/AccountModel.java
@@ -15,22 +15,27 @@ public class AccountModel extends Model {
 		this.repo = r;
 	}
 	
-	public void signup(String id, String pw, Authority authority) {
+	public boolean signup(String id, String pw, Authority authority){
+		if(super.systemManager.getUser() == null) return false;  
+		if(super.systemManager.getUser().getAuthority() != Authority.ADMIN) return false;
+		if(this.repo.contains(id)) return false;
+		
 		switch(authority) {
-//		case PL:
-//			ProjectLeader pl = new ProjectLeader(id, pw);
-//			repo.add(pl);
-//			break;
-//		case DEV:
-//			Dev dev = new Dev(id, pw);
-//			repo.add(dev);
-//			break;
-//		case TESTER:
-//			Tester tester = new Tester(id, pw);
-//			repo.add(tester);
-//			break;
-//		default:
-//			//error
+		case PL:
+			ProjectLeader pl = new ProjectLeader(id, pw);
+			repo.add(pl);
+			break;
+		case DEV:
+			Dev dev = new Dev(id, pw);
+			repo.add(dev);
+			break;
+		case TESTER:
+			Tester tester = new Tester(id, pw);
+			repo.add(tester);
+			break;
+		default:
+			return false;
 		}
+		return true;
 	}
 }
---
+++ SE_Term_Project/src/test/model/AccountModelTest.java
@@ -0,0 +1,50 @@
+package test.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.domain.*;
+import main.domain.enumeration.*;
+import main.model.*;
+import main.repository.MysqlAccountRepo;
+import test.repository.RepoTest;
+
+class AccountModelTest {
+	RepoTest testrepo = new RepoTest();
+	MysqlAccountRepo account_repo = new MysqlAccountRepo();
+	String crateSQL = "create_db.sql";
+	
+	SystemManager sysmanager = new SystemManager(null); 
+	LoginModel login_model = new LoginModel(sysmanager, account_repo);
+	AccountModel account_model = new AccountModel(sysmanager, account_repo);
+	
+	Admin admin = new Admin("admin", "admin");
+	
+    @BeforeEach
+    void beforeEach() {
+    	// 매우중요!!!! 테스트할 때 사용하는 repo는 test.sqlite3로 지정해야 함!!!!!!
+    	testrepo.setJDBC("org.sqlite.JDBC", "jdbc:sqlite:test.sqlite3");
+    	account_repo.setJDBC("org.sqlite.JDBC", "jdbc:sqlite:test.sqlite3");
+    	testrepo.clearDB();
+    	testrepo.createDB(crateSQL);
+    	////////////////////////////////////////////////////////////////////
+    	
+    	// admin 계정 추가
+    	account_repo.add(admin);
+    }
+    
+    @Test
+    void signup() {
+    	login_model.tryLogin("admin", "admin");
+    	account_model.signup("pl1", "pl1", Authority.PL);
+    	account_model.signup("dev1", "dev1", Authority.DEV);
+    	account_model.signup("tester1", "tester1", Authority.TESTER);
+    	
+    	login_model.logout();
+    	login_model.tryLogin("dev1", "dev1");
+    	Assertions.assertEquals("dev1", sysmanager.getUser().getAccountID());
+    }
+}
